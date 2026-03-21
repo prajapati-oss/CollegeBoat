47749e260cafa73638d59477627b92eaf27c6308
prajapati-oss <prajapatirakesh6260@gmail.com>
2026-03-21 16:27:56 +0530

    githubcommit

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
-print("Hello how are you")
-print("Hello main")
-print()
+import streamlit as st
+
+st.write("URL:", st.secrets.get("SUPABASE_URL"))
+st.write("KEY exists:", "SUPABASE_KEY" in st.secrets)
